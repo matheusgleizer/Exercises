c6df072988a06880d4849e08f6b3514840c3dc69
matheusgleizer <matheusgleizer@gmail.com>
2020-08-27 14:16:58 +0200

    14:16 27.08

--- float_layout/server.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const server = express();
-server.use(express.static("/home/dcil164/Documents/groupExercise/exercises/float_layout"));
-const port = 3333;
-server.listen(port, () => console.log(`Server is listening to the port: ${port}`));
